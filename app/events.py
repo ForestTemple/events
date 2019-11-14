6df77caad7c94d3dfe2eec4a7ebd0a7db3338a48
Michael <Michael Serignese>
2019-11-14 08:48:35 -0500

    Created Locations in database and expanded Flask models.

--- app/events.py
+++ app/events.py
@@ -30,7 +30,8 @@ class Events(db.Model):
     name = db.Column('name', db.String(88), nullable=False)
     description = db.Column('description', db.String(88))
     datestamp = db.Column('datestamp', db.DateTime, nullable=False)
-    # here
+    location = db.relationship(
+            'Events_Locations', backref='events', lazy=True, uselist=False)
 
     def __init__(self, eid, email, name, description, datestamp):
         self.eid = eid
@@ -45,11 +46,20 @@ class Events(db.Model):
 class Admins(db.Model):
     uid = db.Column('uid', db.Integer, primary_key = True)
 
-    def __init__(self, uid, email, name, hashed, salt):
+    def __init__(self, uid):
         self.uid = uid
 
     def __repr__(self):
         return '<Admin "{}">'.format(self.uid)
+
+class SuperAdmins(db.Model):
+    uid = db.Column('uid', db.Integer, primary_key = True)
+
+    def __init__(self, uid):
+        self.uid = uid
+
+    def __repr__(self):
+        return '<SuperAdmin "{}">'.format(self.uid)
 
 class RSOs(db.Model):
     rid = db.Column('rid', db.Integer, primary_key=True)
@@ -60,7 +70,115 @@ class RSOs(db.Model):
         self.name = name
 
     def __repr__(self):
-        
+        return '<RSO "{}">'.format(self.name)
+
+class Events_Private(db.Model):
+    eid = db.Column('eid', db.Integer, primary_key=True)
+
+    def __init__(self, eid):
+        self.eid = eid
+
+    def __repr__(self):
+        return '<EventsPrivate "{}">'.format(self.eid)
+
+class Events_RSO(db.Model):
+    eid = db.Column('eid', db.Integer, primary_key=True)
+    rid = db.Column('rid', db.Integer, nullable=False)
+
+    def __init__(self, eid, rid):
+        self.eid = eid
+        self.rid = rid
+
+    def __repr__(self):
+        return '<EventsPrivate "{}">'.format(self.eid)
+
+class Comments(db.Model):
+    cid = db.Column('cid', db.Integer, primary_key=True)
+    eid = db.Column('eid', db.Integer, nullable=False)
+    email = db.Column('email', db.String(88), nullable=False)
+    text = db.Column('text', db.String(88), nullable=False)
+    honor = db.Column('honor', db.Integer)
+    datestamp = db.Column('datestamp', db.DateTime)
+
+    def __init__(self, cid, eid, email, text, honor, datestamp):
+        self.cid = cid
+        self.eid = eid
+        self.email = email
+        self.text = text
+        self.honor = honor
+        self.datestamp = datestamp
+
+    def __repr__(self):
+        return '<Comment (cid={}, eid={}, email="{}")>'.format(
+                self.cid, self.eid, self.email)
+
+class Universities(db.Model):
+    unid = db.Column('unid', db.Integer, primary_key=True)
+    num_students = db.Column('num_students', db.Integer)
+    name = db.Column('name', db.String(88), nullable=False)
+    description = db.Column('description', db.String(200))
+
+    def __init__(self, unid, num_students, name, description):
+        self.unid = unid
+        self.num_students = num_students
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return '<University "{}">'.format(self.name)
+
+class Universities_Pictures(db.Model):
+    unid = db.Column('unid', db.Integer)
+    file_path = db.Column('file_path', db.String(50), primary_key=True)
+
+    def __init__(self, unid, file_path):
+        self.unid = unid
+        self.file_path = file_path
+
+    def __repr__(self):
+        return '<UniversitiesPictures "{}">'.format(self.file_path)
+
+class Locations(db.Model):
+    lid = db.Column('lid', db.Integer, primary_key=True)
+    latitude = db.Column('latitude', db.Float, nullable=False)
+    longitude = db.Column('longitude', db.Float, nullable=False)
+    name = db.Column('name', db.String(88))
+
+    def __init__(self, lid, latitude, longitude, name):
+        self.lid = lid
+        self.latitude = latitude
+        self.longitude = longitude
+        self.name = name
+
+    def __repr__(self):
+        return '<Location (lat={}, lon={}, name="{}")>'.format(
+                self.latitude, self.longitude, self.name)
+
+class Universities_Locations(db.Model):
+    lid = db.Column('lid', db.Integer, primary_key=True)
+    unid = db.Column('unid', db.Integer, nullable=False)
+
+    def __init__(self, lid, unid):
+            self.lid = lid
+            self.unid = unid
+     
+    def __repr__(self):
+        return '<UniversityLocation (lid={}, unid={})>'.format(
+                self.lid, self.unid)
+
+class Events_Locations(db.Model):
+    lid = db.Column('lid', db.Integer, 
+            db.ForeignKey('locations.lid'), primary_key=True)
+    eid = db.Column('eid', db.Integer,
+            db.ForeignKey('events.eid'), nullable=False)
+
+    def __init__(self, lid, eid):
+            self.lid = lid
+            self.eid = eid
+     
+    def __repr__(self):
+        return '<EventsLocation (lid={}, eid={})>'.format(
+                self.lid, self.eid)
 
 @app.route('/')
 def home():
--- db/inputs.sql
+++ db/inputs.sql
@@ -1,18 +1,47 @@
 INSERT INTO Users (email, name, hash, salt) VALUES
 	('mike@knights.ucf.edu', 'Michael', '293481-59121sdfasf', 'asd2344234423'),
-	('jojo-kang@knights.ucf.edu', 'jojo', '293481-1sdfasf', 'as4234423')
+	('donovan@knights.ucf.edu', 'Donovan', '293481-1sdfasf', 'as4234423'),
+	('seth@ucla.edu', 'Seth', '123492334222342423', '12342134123')
 ;
 
-INSERT INTO SuperAdmins (UID) VALUES
+INSERT INTO SuperAdmins (uid) VALUES
 	(1)
 ;
 
+INSERT INTO Admins (uid) VALUES
+	(2)
+;
+
 INSERT INTO Events (email, name, description, phone, datestamp) VALUES
-	('mike@knights.ucf.edu', 'Michael', 'This is a fun event', '561-545-4324', '20191231T153532')
+	('mike@knights.ucf.edu', 'Michael', 'This is a fun PRIVATE event', '561-545-4324', '20191231T153532'),
+	('mike@knights.ucf.edu', 'Michael', 'This is also a very fun event', '561-545-4324', '20191125T153532')
+;
+
+INSERT INTO Events_Private (eid) VALUES
+	(1)
+;
+
+INSERT INTO Comments (email, text, honor, eid, datestamp) VALUES
+	('mike@knights.ucf.edu', 'Hi this is a PRIVATE comment.', '5', '1', '20191125'),
+	('mike@knights.ucf.edu', 'Hi this is a comment.', '5', '2', '20191125T1535')
 ;
 
 INSERT INTO Universities (num_students, name, description) VALUES
-	(150, 'The University of Hunter-Gatherers', 'We hunt our own.')
+	(50000, 'The University of Central Florida', 'A great school with a great price.'),
+	(150, 'The University of UCLA', 'Student debt and more.'),
+	(60000, 'The University of Hunter-Gatherers', 'We do not hunt our own.')
+;
+
+INSERT INTO Locations (latitude, longitude, name) VALUES
+	(7.2323, 234.4342, 'UCF Walled Garden')
+;
+
+INSERT INTO Universities_Locations (lid, unid) VALUES
+	(1, 1)
+;
+
+INSERT INTO Events_Locations (lid, eid) VALUES
+	(1, 1)
 ;
 
 INSERT INTO Universities_Pictures (file_path) VALUES
